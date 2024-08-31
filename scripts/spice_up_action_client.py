a28ff0009b4ca8eaa2f85ddc5f92f65eb777e420
jau <jau.gretler@gmail.com>
2024-08-31 19:44:49 +0200

    action infra works

--- scripts/spice_up_action_client.py
+++ scripts/spice_up_action_client.py
@@ -1,42 +1,40 @@
 #! /usr/bin/env python
 
 import rospy
-from __future__ import print_function
-
-# Brings in the SimpleActionClient
+import sys
 import actionlib
 
-# Brings in the messages used by the fibonacci action, including the
-# goal message and the result message.
-import actionlib_tutorials.msg
+from spice_up_coordinator.msg import SpiceUpBottlePickAction,SpiceUpBottlePickGoal
 
-def fibonacci_client():
+
+def spice_up_action_client():
     # Creates the SimpleActionClient, passing the type of the action
-    # (FibonacciAction) to the constructor.
-    client = actionlib.SimpleActionClient('fibonacci', actionlib_tutorials.msg.FibonacciAction)
+    # (SpiceUpBottlePickAction) to the constructor.
+    client = actionlib.SimpleActionClient('spice_up_action_server', SpiceUpBottlePickAction)
 
-    # Waits until the action server has started up and started
-    # listening for goals.
     client.wait_for_server()
 
     # Creates a goal to send to the action server.
-    goal = actionlib_tutorials.msg.FibonacciGoal(order=20)
+    goal = SpiceUpBottlePickGoal()
+    goal.activation = True
 
     # Sends the goal to the action server.
     client.send_goal(goal)
+    print("[SpiceUpActionClient] :  Goal sent")
 
     # Waits for the server to finish performing the action.
     client.wait_for_result()
 
     # Prints out the result of executing the action
-    return client.get_result()  # A FibonacciResult
+    return client.get_result()  # A SpiceUpBottlePickActionResult
+
 
 if __name__ == '__main__':
     try:
-        # Initializes a rospy node so that the SimpleActionClient can
-        # publish and subscribe over ROS.
-        rospy.init_node('fibonacci_client_py')
-        result = fibonacci_client()
-        print("Result:", ', '.join([str(n) for n in result.sequence]))
+        rospy.init_node('spice_up_action_client')
+        print("[SpiceUpActionClient] :  Initialized")
+        result = spice_up_action_client()
+        print("ee_pickup_target: "+str(result.ee_pickup_target))
+        print("ee_dropoff_target: "+str(result.ee_dropoff_target))
     except rospy.ROSInterruptException:
         print("program interrupted before completion", file=sys.stderr)
--- scripts/spice_up_action_server.py
+++ scripts/spice_up_action_server.py
@@ -12,7 +12,7 @@ from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import Image, CameraInfo
 
-from spice_up_coordinator.msg import SpiceUpBottlePickAction,SpiceUpBottlePickActionResult
+from spice_up_coordinator.msg import SpiceUpBottlePickAction,SpiceUpBottlePickResult
 from spice_up_coordinator.srv._SpiceName import SpiceName, SpiceNameRequest
 from spice_up_coordinator.srv._IDXAcquisition import IDXAcquisition,IDXAcquisitionRequest
 
@@ -32,16 +32,17 @@ class spiceUpCoordinator:
         print("[spiceUpCoordinator] : Waiting for idx_finder_server...")
         rospy.wait_for_service('idx_finder_server')
 
-        rospy.loginfo("[spiceUpCoordinator] : "+str("Initialized"))
+        print("[spiceUpCoordinator] : "+str("Initialized"))
         
 
     def execute_cb(self, goal): # Goal of type: SpiceUpBottlePickGoal
         
         # publish info to the console for the user
-        rospy.loginfo("[spiceUpCoordinator] : Received goal")
+        print("[spiceUpCoordinator] : Received goal")
         
-        result = SpiceUpBottlePickActionResult()
-        result.ee_pickup_target = PoseStamped
+        result = SpiceUpBottlePickResult()
+        result.ee_pickup_target = PoseStamped()
+        result.ee_dropoff_target.header.stamp = rospy.Time.now()
         result.ee_dropoff_target = self.p_first_drop_off
 
         if goal.activation:       
@@ -49,31 +50,42 @@ class spiceUpCoordinator:
             # Send request for color_profile to color_profile_server
             spice_name_request = SpiceNameRequest()
             spice_name_service_handle = rospy.ServiceProxy('spice_name_server', SpiceName)
+            print("[spiceUpCoordinator] : Requesting target spice name")
             spice_name_service_response = spice_name_service_handle(spice_name_request)
-
+            
             target_spice = spice_name_service_response.spice_name
+            print("[spiceUpCoordinator] : Received target spice name: "+str(target_spice))
 
             # Send request for target spice position idx to idx_finder_server
             idx_request = IDXAcquisitionRequest()
             idx_request.target_spice = target_spice
             idx_acquisition_service_handle = rospy.ServiceProxy('idx_finder_server', IDXAcquisition)
+            print("[spiceUpCoordinator] : Requesting target spice IDX")
             idx_acquisition_service_response = idx_acquisition_service_handle(idx_request)
 
             target_idx = idx_acquisition_service_response.idx
+            print("[spiceUpCoordinator] : Received target spice IDX: "+str(target_idx))
 
             result.ee_pickup_target = self.poses[target_idx]
 
-            return result
+            self.action_server.set_succeeded(result)
     
     def load_params(self):
         self.pick_up_pose = None
         self.place_pose = None
 
         # Possible grasping poses at 4 known locations in Kallax shelf TODO!!
-        self.p0 = PoseStamped
-        self.p1 = PoseStamped
-        self.p2 = PoseStamped
-        self.p3 = PoseStamped
+        self.p0 = PoseStamped()
+        self.p0.header.stamp = rospy.Time.now()
+
+        self.p1 = PoseStamped()
+        self.p1.header.stamp = rospy.Time.now()
+
+        self.p2 = PoseStamped()
+        self.p2.header.stamp = rospy.Time.now()
+        
+        self.p3 = PoseStamped()
+        self.p3.header.stamp = rospy.Time.now()
 
         self.poses = {
             0: self.p0,
@@ -82,8 +94,11 @@ class spiceUpCoordinator:
             3: self.p3
         }
 
-        self.p_first_drop_off = PoseStamped
-        self.p_second_drop_off = PoseStamped
+        self.p_first_drop_off = PoseStamped()
+        self.p_first_drop_off.header.stamp = rospy.Time.now()
+
+        self.p_second_drop_off = PoseStamped()
+        self.p_second_drop_off.header.stamp = rospy.Time.now()
         
         
 if __name__ == '__main__':
